12a1d0c8402b82a3a2c9a088c538a7992f87b4f0
Nicholas Schaefer <nick@nicholasschaefer.com>
2023-09-25 18:23:10 +0000

    completed exercise easy_1-6

---
+++ 1_lesson_preparations/small_problems/easy_1/6_right_triangles/app.rb
@@ -0,0 +1,20 @@
+def triangle length_of_string
+
+    length_of_string.times do |i|
+        spaces_str = ""
+        stars_str = ""
+        full_line = ""
+
+        count_blank_spaces_to_add = length_of_string - (i +1)
+        spaces_str = " " * count_blank_spaces_to_add
+
+        count_stars_to_add = i + 1
+        stars_str = "*" * count_stars_to_add
+
+        full_line = spaces_str + stars_str
+
+        puts full_line
+    end
+end
+
+triangle 6
