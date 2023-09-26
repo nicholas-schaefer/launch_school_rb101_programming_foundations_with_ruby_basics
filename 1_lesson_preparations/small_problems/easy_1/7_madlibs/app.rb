417d75ff601d0eaeebb42e70256c46f2a965f7c1
Nicholas Schaefer <nick@nicholasschaefer.com>
2023-09-26 12:48:26 +0000

    completed exercise easy_1-7

---
+++ 1_lesson_preparations/small_problems/easy_1/7_madlibs/app.rb
@@ -0,0 +1,13 @@
+puts ">> Enter a noun"
+noun = gets.chomp.to_s
+
+puts ">> Enter verb"
+verb = gets.chomp.to_s
+
+puts ">> Enter an adjective"
+adjective = gets.chomp.to_s
+
+puts ">> Enter an adverb"
+adverb = gets.chomp.to_s
+
+puts "Do you #{verb} your #{adjective} #{noun} #{adverb}? That's hilarious!"
